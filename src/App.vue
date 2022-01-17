3889f7bc7eb9fdc8578d130b547209b00ef86aa9
AndyBeable <andy.beable@gmail.com>
2022-01-17 15:47:13 +0000

    header initial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@100;300;400;500;700;900&family=IBM+Plex+Sans:wght@400;700&family=Kumbh+Sans:wght@400;700&family=Outfit:wght@300&family=Rubik:wght@400;500;700&display=swap');
+
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  font-family: 'Rubik', sans-serif;
 }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <div class="header__search">
+      <div class="search__container">
+        <h1>IP Address Tracker</h1>
+        <div class="search__input">
+          <input type="text">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  flex-direction: column;
+  height: 25vh;
+  max-height: 25vh;
+  background-image: url('~@/assets/pattern-bg.png');
+  background-size: cover;
+  padding-top: 10px;
+}
+
+.search__container {
+  width: 500px;
+  margin: 0 auto;
+}
+
+.search__input {
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+
+input {
+  width: 100%;
+  padding: 3px 2px;
+  border: none;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  height: 30px;
+}
+input:focus {
+  outline: none;
+}
+  h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 32px;
+    padding-bottom: 4px;
+    font-weight: 200;
+  }
+
+
+</style>
