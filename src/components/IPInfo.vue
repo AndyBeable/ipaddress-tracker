0664248f73800d0a181b259aa211fba2bd0f73a7
AndyBeable <andy.beable@gmail.com>
2022-01-17 17:02:54 +0000

    first render

---
+++ src/components/IPInfo.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="results">
+  <div class="stat">
+    <h3>IP Address</h3>
+    <span>8.8.8.8</span>
+  </div>
+  <div class="stat">
+    <h3>Location</h3>
+    <span>London</span>
+  </div>
+  <div class="stat">
+    <h3>Timezone</h3>
+    <span>UTC 07:00</span>
+  </div>
+  <div class="stat">
+    <h3>ISP</h3>
+    <span>Google</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "IPInfo"
+};
+</script>
+
+<style lang="scss" scoped>
+.results {
+  position: absolute;
+  display: flex;
+  background-color: #fff;
+  gap: 5px;
+  padding: 8px 8px 12px 8px;
+}
+
+.stat {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,21 @@
       <div class="search__container">
         <h1>IP Address Tracker</h1>
         <div class="search__input">
-          <input type="text">
+          <input type="text" placeholder="Search for any IP address or leave empty to get your ip info"/>
+          <i class="fas fa-chevron-right chevron"></i>
         </div>
+        <IPInfo />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import IPInfo from "@/components/IPInfo";
+
 export default {
   name: "Home",
-  components: {},
+  components: { IPInfo },
 };
 </script>
 
@@ -22,9 +26,9 @@ export default {
 .header {
   display: flex;
   flex-direction: column;
-  height: 25vh;
-  max-height: 25vh;
-  background-image: url('~@/assets/pattern-bg.png');
+  height: 30vh;
+  max-height: 30vh;
+  background-image: url("~@/assets/pattern-bg.png");
   background-size: cover;
   padding-top: 10px;
 }
@@ -51,13 +55,29 @@ input {
 input:focus {
   outline: none;
 }
-  h1 {
-    color: #fff;
-    text-align: center;
-    font-size: 32px;
-    padding-bottom: 4px;
-    font-weight: 200;
-  }
 
+input::placeholder {
+  padding-left: 5px;
+  font-size: 16px;
+  color: #b0afaf;
+}
+h1 {
+  color: #fff;
+  text-align: center;
+  font-size: 28px;
+  padding-bottom: 10px;
+  font-weight: 200;
+}
 
+.chevron {
+  cursor: pointer;
+  background: #000;
+  color: #fff;
+  padding-left: 4px;
+  padding-right: 4px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  display: flex;
+  align-items: center;
+}
 </style>
