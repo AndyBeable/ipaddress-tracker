40098251ca0cb628a4d3e08d744a8d58f90e7efc
AndyBeable <andy.beable@gmail.com>
2022-01-18 16:44:10 +0000

    leaflet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,49 @@
       <div class="search__container">
         <h1>IP Address Tracker</h1>
         <div class="search__input">
-          <input type="text" placeholder="Search for any IP address or leave empty to get your ip info"/>
+          <input
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+          />
           <i class="fas fa-chevron-right chevron"></i>
         </div>
         <IPInfo />
       </div>
+      <div id="map" class="map"></div>
     </div>
   </div>
 </template>
 
 <script>
 import IPInfo from "@/components/IPInfo";
+import { onMounted } from "vue";
+import leaflet from "leaflet";
 
 export default {
   name: "Home",
   components: { IPInfo },
+  setup() {
+    let myMap;
+    onMounted(() => {
+      myMap = leaflet.map("map").setView([51.505, -0.09], 13);
+
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5keWJlYWJsZSIsImEiOiJja2tvNHFsbzcwemdzMm9uc3d3cmNudjB4In0.YRvihj2bTJkl4z1zTpbByw",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1IjoiYW5keWJlYWJsZSIsImEiOiJja2tvNHFsbzcwemdzMm9uc3d3cmNudjB4In0.YRvihj2bTJkl4z1zTpbByw",
+          }
+        )
+        .addTo(myMap);
+    });
+  },
 };
 </script>
 
@@ -69,7 +97,6 @@ input::placeholder {
   color: #b0afaf;
 }
 
-
 .chevron {
   cursor: pointer;
   background: #000;
@@ -79,5 +106,10 @@ input::placeholder {
   border-bottom-right-radius: 16px;
   display: flex;
   align-items: center;
+}
+
+.map {
+  height: 100%;
+  z-index: 10;
 }
 </style>
